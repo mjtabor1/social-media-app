991b09834e17c8d3221580cb9536307e13269674
Michael Tabor <mjtabor23@gmail.com>
2025-05-28 21:21:05 -0600

    client-setup: initial scaffolding, setup home route, api proxy service, add material ui packages

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home Page</div>} />
+    </Routes>
+  );
+}
+
+export default App;
